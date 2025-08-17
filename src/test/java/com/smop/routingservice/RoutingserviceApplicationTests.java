a44930cff78d29d338fddd5ec0e6eb97fa05dcfa
Robertino Solanas <r.sol@gmx.de>
2025-08-17 13:37:16 +0200

    initialzing

---
+++ src/test/java/com/smop/routingservice/RoutingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smop.routingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
